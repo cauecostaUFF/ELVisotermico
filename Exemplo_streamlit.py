fca9af1193dec325c6113b5c393fa13bd126f822
Caue Costa <cauecosta@id.uff.br>
2022-10-26 15:25:29 -0300

    Quinta versão

--- Exemplo_streamlit.py
+++ Exemplo_streamlit.py
@@ -317,7 +317,7 @@ plt.show()
 st.pyplot(plt)
 if st.session_state["Sistema"] and sist == -1:
     texto = st.session_state["Sistema"]
-    texto = texto[0:-2]
+    texto = texto[0:-1]
     st.write("Dados experimentais do "+ texto.lower()+" K")
 col_end1,col_end2,col_end3 = st.columns(3)
 with col_end3:
